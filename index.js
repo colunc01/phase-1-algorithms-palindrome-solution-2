28c1cd3e51a5ee8191b305da6b3450f9352fa55b
Cesar Colunga <cesara.colunga@gmail.com>
2023-04-03 14:44:37 -0500

    done

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
+
+function reverseString(str) {
+    //make it concise
+    return str.split("").reverse().join("");
+}
+
 
 /* 
   Add your pseudocode here
+  string1 = argument
+  string2 = reverseArgument
+
+  if argument = reverseArgument then string is palindrome
+  else it is not 
 */
 
 /*
   Add written explanation of your solution here
+  I figured spliting the string to an array and then reversing it would be the most efficient way to solve this code.
 */
 
 // You can run `node index.js` to view these console logs
